709be1ff04b6ad8e45db1df13f00230569ff85ed
Ethan Long <ethan.long@vertical-knowledge.com>
2019-02-28 14:22:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+The Band Allegiance
+=====================
+
+### Run
+
+```
+npm install
+npm start
+open http://localhost:3000
+```
---
+++ src/Landing.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+@observer
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        asdf
+      </div>
+    );
+  }
+};
+
+export default Landing;
